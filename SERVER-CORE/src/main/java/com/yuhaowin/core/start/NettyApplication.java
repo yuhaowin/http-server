a7b43474d96a5c3acfec84fa163441dcad417264
yuhaowin <yuhao.coding@gmail.com>
2020-08-06 01:13:21 +0800

    delete controller

--- SERVER-CORE/src/main/java/com/yuhaowin/core/start/NettyApplication.java
+++ SERVER-CORE/src/main/java/com/yuhaowin/core/start/NettyApplication.java
@@ -5,19 +5,20 @@ import com.yuhaowin.core.handler.BeanFactory;
 import com.yuhaowin.core.handler.HandlerManager;
 import com.yuhaowin.core.server.HttpServer;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class NettyApplication {
+
     public static void run(Class<?> clazz, String[] args) {
         System.out.println("Hello NettyApplication");
-        //int port = 8080; //默认启动端口
-        //String commandPort = Arrays.stream(args)
-        //                .filter(arg -> arg.startsWith("--server.port=")).findFirst().get();
-        //if (commandPort != null && !commandPort.isEmpty()){
-        //   port = Integer.valueOf(commandPort.split("=")[1]);
-        //}
-        // todo 将命令行产生传递给 httpServer
-        HttpServer httpServer = new HttpServer(8080);
+        int port = 8080; //默认启动端口
+        String commandPort = Arrays.stream(args)
+                        .filter(arg -> arg.startsWith("--server.port=")).findFirst().get();
+        if (commandPort != null && !commandPort.isEmpty()){
+           port = Integer.valueOf(commandPort.split("=")[1]);
+        }
+        HttpServer httpServer = new HttpServer(port);
         try {
             // 扫描指定包路径下的所有 class
             List<Class<?>> classList = ClassScanner.scanClass(clazz.getPackage().getName());
@@ -28,5 +29,10 @@ public class NettyApplication {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        HttpServer httpServer = new HttpServer(8080);
+        httpServer.start();
     }
 }
